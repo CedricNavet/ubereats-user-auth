a85052c4ba5eb4cb8246e8e0f86b0a60aee3e2fb
CEDRIC NAVET <cedric.navet@viacesi.fr>
2021-07-02 10:07:02 +0200

    cpu charge

--- Controllers/CPUServer.cs
+++ Controllers/CPUServer.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ubereats_user_auth.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace ubereats_user_auth.Controllers
 {
@@ -14,6 +17,13 @@ namespace ubereats_user_auth.Controllers
     [ApiController]
     public class CPUServer : ControllerBase
     {
+
+        private readonly UberEatsAuthDBContext _context;
+
+        public CPUServer(UberEatsAuthDBContext context)
+        {
+            _context = context;
+        }
         // POST: api/auth/login
         [HttpGet]
         public async Task<ActionResult<string>> CPUCharge()
@@ -22,6 +32,42 @@ namespace ubereats_user_auth.Controllers
             var mem = proc.WorkingSet64;
             var cpu = proc.TotalProcessorTime;
             return (mem / 1024.0).ToString();
+
+        }
+
+        [HttpGet("Charge")]
+        public async Task<ActionResult<Stats>> Charge()
+        {
+            var startTime = DateTime.UtcNow;
+            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var stopWatch = new Stopwatch();
+            // Start watching CPU
+            stopWatch.Start();
+
+            // Meansure something else, such as .Net Core Middleware
+            var users = await _context.Users.ToListAsync();
+
+            // Stop watching to meansure
+            stopWatch.Stop();
+            var endTime = DateTime.UtcNow;
+            var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+
+            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+
+            var cpuUsagePercentage = cpuUsageTotal * 100;
+
+            var proc = Process.GetCurrentProcess();
+            var mem = proc.WorkingSet64;
+            var cpu = proc.TotalProcessorTime;
+            Stats stats = new Stats()
+            {
+                CPU_Usage = cpuUsagePercentage,
+                Mem_Process = (mem / 1024.0),
+                Process_Time = cpuUsedMs,
+            };
+            return stats;
 
         }
     }
---
+++ Model/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ubereats_user_auth.Model
+{
+    public class Stats
+    {
+        public double CPU_Usage { get; set; }
+        public double Mem_Process { get; set; }
+        public double Process_Time { get; set; }
+    }
+}
